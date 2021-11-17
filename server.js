2444e3435446169893a57a57fd2f304c46792331
mariapaolacanali <mariapaola.canali@mail.polimi.it>
2021-11-17 12:55:39 +0100

    added broadcast

--- server.js
+++ server.js
@@ -13,10 +13,19 @@ console.log("server is running on http://localhost:" + port);
 app.use(express.static("public"));
 
 let serverSocket = require("socket.io");
+
 let io = serverSocket(server);
 
 io.on("connection", newConnection);
 
 function newConnection(newSocket) {
   console.log(newSocket.id);
+
+  newSocket.on("mouse", mouseMessage);
+
+  function mouseMessage(dataReceived) {
+    console.log(dataReceived);
+
+    newSocket.broadcast.emit("mouseBroadcast", dataReceived);
+  }
 }
